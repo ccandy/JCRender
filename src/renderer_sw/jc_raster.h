ea4c7292e99e66b0cc05388744313ad2a2fe4c09
ccandy <ccunandy2001@gmail.com>
2026-01-07 20:45:39 -0800

    add jc_raster.h

---
+++ src/renderer_sw/jc_raster.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include<stdint.h>
+
+typedef struct JC_V2
+{
+    float x, y;
+} JC_V2;
+
+typedef struct JC_Color4
+{
+    float r, g, b, a;
+} JC_Color4;
+
+typedef struct JC_Vertex2D
+{
+    JC_V2       pos;
+    JC_Color4   color;
+};
+
+void jc_raster_tri_color_rgba8(
+    uint32_t* color,
+    int width,
+    int height,
+    JC_Vertex2D v0,
+    JC_Vertex2D v1,
+    JC_Vertex2D v2
+);
+
+
