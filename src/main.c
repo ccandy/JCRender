fe06bad03998b391eab7350521a6883dab7bd2dd
ccandy <ccunandy2001@gmail.com>
2026-01-10 18:23:39 -0800

    update main

--- src/main.c
+++ src/main.c
@@ -1,91 +1,69 @@
+// main.c
 #include <SDL2/SDL.h>
 #include <stdint.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include "renderer_sw/jc_raster.h"
 
-static inline uint32_t pack_rbga8(uint8_t r, uint8_t g, uint8_t b, uint8_t a)
+
+static void clear_rgba8(uint32_t* pixels, int w, int h, uint32_t rgba8)
 {
-    return ((uint32_t) r) | ((uint32_t) g << 8) | ((uint32_t) b << 16) | ((uint32_t) a << 24);
+    for (int i = 0; i < w * h; ++i) pixels[i] = rgba8;
 }
 
 int main(int argc, char** argv)
 {
-    (void) argc; (void) argv;
+    (void)argc; (void)argv;
 
-    if(SDL_Init(SDL_INIT_VIDEO)!= 0)
-    {
-        fprintf(stderr, "SDL failed to init");
-        return 1;
-    }
+    const int W = 960;
+    const int H = 540;
 
-    const int W = 960, H = 540;
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) return 1;
 
-    SDL_Window* win = SDL_CreateWindow("JianJian Render", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, W, H, 0);
-    if(!win)
-    {
-        fprintf(stderr, "create window failed");
-        return 1;
-    }
+    SDL_Window* win = SDL_CreateWindow(
+        "JCRender",
+        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+        W, H,
+        0);
 
-    SDL_Renderer* render = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED);
-    if(!render)
-    {
-        fprintf(stderr, "create render failed");
-        return 1;
-    }
-    
-    //use a render texture as the screen
-    SDL_Texture* tex = SDL_CreateTexture(render, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_STREAMING, W, H);
-    if(!tex)
-    {
-        fprintf(stderr, "create tex failed");
-        return 1;
-    }
+    SDL_Renderer* ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED);
+    SDL_Texture* tex = SDL_CreateTexture(
+        ren,
+        SDL_PIXELFORMAT_RGBA8888,   
+        SDL_TEXTUREACCESS_STREAMING,
+        W, H);
 
-    uint32_t* color = (uint32_t*) SDL_malloc((size_t)W * (size_t)H * sizeof(uint32_t));
-    if(!color)
-    {
-        fprintf(stderr, "create color failed");
-        return 1;
-    }
+    uint32_t* framebuffer = (uint32_t*)SDL_malloc((size_t)W * (size_t)H * sizeof(uint32_t));
+    if (!framebuffer) return 1;
 
     bool running = true;
-    uint32_t t0 = SDL_GetTicks();
-
-    while(running)
+    while (running)
     {
         SDL_Event e;
-
-        while(SDL_PollEvent(&e))
+        while (SDL_PollEvent(&e))
         {
-            if(e.type == SDL_QUIT) running = false;
-            if(e.type == SDL_KEYDOWN && e.key.keysym.sym == SDLK_ESCAPE) running = false;
+            if (e.type == SDL_QUIT) running = false;
+            if (e.type == SDL_KEYDOWN && e.key.keysym.sym == SDLK_ESCAPE) running = false;
         }
 
-        uint32_t t = SDL_GetTicks() - t0;
+        clear_rgba8(framebuffer, W, H, jc_pack_rgba8(0.08f, 0.08f, 0.10f, 1.0f));
 
-        for(int j = 0; j < H; j++)
-        {
-            for(int i = 0; i < W; i++)
-            {
-                uint8_t r = (uint8_t)((i + (t / 10)) & 255);
-                uint8_t g = (uint8_t)((i + (t / 15)) & 255);
-                uint8_t b = (uint8_t)((i^j) & 255);
-                color[j * W + i] = pack_rbga8(r, g, b, 255);
-            }
-        }
+        JC_Vertex2D v0 = { .pos = { 100.0f,  80.0f }, .color = { 1, 0, 0, 1 } };
+        JC_Vertex2D v1 = { .pos = { 800.0f, 120.0f }, .color = { 0, 1, 0, 1 } };
+        JC_Vertex2D v2 = { .pos = { 300.0f, 460.0f }, .color = { 0, 0, 1, 1 } };
 
-        SDL_UpdateTexture(tex, NULL, color, W * (int)sizeof(uint32_t));
-        SDL_RenderClear(render);
-        SDL_RenderCopy(render, tex, NULL, NULL);
-        SDL_RenderPresent(render);
+        jc_raster_tri_color_rgba8(framebuffer, W, H, v0, v1, v2);
+
+        SDL_UpdateTexture(tex, NULL, framebuffer, W * (int)sizeof(uint32_t));
+        SDL_RenderClear(ren);
+        SDL_RenderCopy(ren, tex, NULL, NULL);
+        SDL_RenderPresent(ren);
     }
 
-    SDL_free(color);
+    SDL_free(framebuffer);
     SDL_DestroyTexture(tex);
-    SDL_DestroyRenderer(render);
+    SDL_DestroyRenderer(ren);
     SDL_DestroyWindow(win);
     SDL_Quit();
-
     return 0;
 }
